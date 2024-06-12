455875f8c794dacd0f57ea200145f324879d2600
Raydo Matthee <raydo@skunkworks.africa>
2024-06-12 04:44:06 +0200

    index.html updated

--- learning-paths/learning-paths/match_courses_to_bages.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def match_courses_to_badges():
-    # Your logic to match courses to badges goes here
-    print("Matching courses to badges...")
-
-if __name__ == "__main__":
-    match_courses_to_badges()
