010a128611718bc3dac7aa91b560f9b52a5689e0
burnt-exe <raydo@skunkworks.africa>
2024-06-16 01:39:07 +0200

    changes to css, js scripts added

--- add_header_include.py
+++ add_header_include.py
@@ -1,45 +1,39 @@
 import os
 
-def add_header_include(directory, header_file):
-    # Traverse the directory tree
-    for root, dirs, files in os.walk(directory):
+def update_html_files_with_header(header_file, base_directory):
+    # Read the content of the header file
+    with open(header_file, 'r', encoding='utf-8') as file:
+        header_content = file.read()
+
+    # Loop through all directories and files in the base directory
+    for root, dirs, files in os.walk(base_directory):
         for filename in files:
-            if filename.endswith(".html"):
+            if filename.endswith('.html') and filename != 'header.html':
                 filepath = os.path.join(root, filename)
-                
-                # Skip the header.html file
-                if filepath == header_file:
-                    continue
-                
-                # Read the content of the HTML file
+
                 with open(filepath, 'r', encoding='utf-8') as file:
-                    lines = file.readlines()
-                
-                # Check if the header include line is already present
-                include_line = "@@include('partials/header.html')"
-                if any(include_line in line for line in lines):
-                    print(f"'header.html' include already present in {filepath}")
-                    continue
-                
-                # Find the index of the <body> tag
-                body_index = -1
-                for i, line in enumerate(lines):
-                    if "<body>" in line:
-                        body_index = i
-                        break
-                
-                # If <body> tag is found, insert the include line
-                if body_index != -1:
-                    lines.insert(body_index + 1, f"    {include_line}\n")
-                    # Write the modified content back to the HTML file
-                    with open(filepath, 'w', encoding='utf-8') as file:
-                        file.writelines(lines)
-                    print(f"Added 'header.html' include to {filepath}")
-                else:
-                    print(f"<body> tag not found in {filepath}")
+                    content = file.readlines()
 
-# Set the directory where your HTML files are located
-html_directory = "./"
-header_filepath = os.path.join(html_directory, "partials/header.html")
+                # Find the location to insert the header
+                new_content = []
+                inside_header = False
+                for line in content:
+                    if '<header>' in line:
+                        inside_header = True
+                        new_content.append(header_content + '\n')
+                    elif '</header>' in line:
+                        inside_header = False
+                        continue
+                    elif not inside_header:
+                        new_content.append(line)
 
-add_header_include(html_directory, header_filepath)
+                with open(filepath, 'w', encoding='utf-8') as file:
+                    file.writelines(new_content)
+
+                print(f'Updated {filepath}')
+
+# Update the base_directory with the path to your project's root directory
+base_directory = './'
+header_file = os.path.join(base_directory, 'partials/header.html')
+
+update_html_files_with_header(header_file, base_directory)
