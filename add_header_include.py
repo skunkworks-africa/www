5988016e670ad85c1ff2ee380c31abca2f3afd51
burnt-exe <raydo@skunkworks.africa>
2024-06-16 05:50:54 +0200

    dir updates to .htmls files

--- add_header_include.py
+++ add_header_include.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 def update_html_files_with_header(header_file, base_directory):
     # Read the content of the header file
@@ -13,51 +12,31 @@ def update_html_files_with_header(header_file, base_directory):
                 filepath = os.path.join(root, filename)
 
                 with open(filepath, 'r', encoding='utf-8') as file:
-                    content = file.readlines()
+                    content = file.read()
 
-                # Remove the @@include directive and find the location to insert the header
-                new_content = []
-                include_found = False
-                for line in content:
-                    if '@@include(\'partials/header.html\')' in line:
-                        include_found = True
-                        new_content.append(header_content + '\n')
-                    else:
-                        new_content.append(line)
+                # Remove existing header and @@include('partials/header.html')
+                content = content.replace(header_content, '')
+                content = content.replace("@@include('partials/header.html')", '')
 
-                # Write the updated content back to the file
+                # Find the location to insert the new header
+                header_start = content.find('<header>')
+                header_end = content.find('</header>') + len('</header>')
+
+                if header_start != -1 and header_end != -1:
+                    # Remove the existing header
+                    content = content[:header_start] + content[header_end:]
+
+                # Insert the new header at the top of the body
+                body_start = content.find('<body>') + len('<body>')
+                new_content = content[:body_start] + '\n' + header_content + '\n' + content[body_start:]
+
                 with open(filepath, 'w', encoding='utf-8') as file:
-                    file.writelines(new_content)
+                    file.write(new_content)
 
                 print(f'Updated {filepath}')
-
-def check_partner_logos(base_directory, partners_dir):
-    # List of required logo dimensions
-    dimensions = {
-        'favicon': (16, 16),
-        'logo': (100, 100),
-        'badge': (200, 200)
-    }
-
-    # Loop through all partners in the partners directory
-    for partner in os.listdir(partners_dir):
-        partner_path = os.path.join(partners_dir, partner)
-        if os.path.isdir(partner_path):
-            for size_name, size in dimensions.items():
-                logo_path = os.path.join(partner_path, f'{partner}_{size_name}.png')
-                if not os.path.exists(logo_path):
-                    print(f'Logo {logo_path} is missing, generating placeholder...')
-                    # Generate a placeholder image if the logo is missing
-                    from PIL import Image, ImageDraw
-                    img = Image.new('RGB', size, color='grey')
-                    draw = ImageDraw.Draw(img)
-                    draw.text((10, 10), f'{partner} {size_name}', fill='white')
-                    img.save(logo_path)
 
 # Update the base_directory with the path to your project's root directory
 base_directory = './'
 header_file = os.path.join(base_directory, 'partials/header.html')
-partners_dir = os.path.join(base_directory, 'partners')
 
 update_html_files_with_header(header_file, base_directory)
-check_partner_logos(base_directory, partners_dir)
