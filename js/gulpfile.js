9023b1831b577c70b350288d812f5e5b06775709
Raydo Matthee <raydo@skunkworks.africa>
2024-07-05 05:58:23 +0200

    Update gulpfile.js
    Signed-off-by: Raydo Matthee <raydo@skunkworks.africa>

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -2,12 +2,12 @@ const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 
 gulp.task('file-include', async function() {
-  gulp.src(['./src/**/*.html', '!./src/partials/**'])
+  gulp.src(['./**/*.html', '!./partials/**'])
     .pipe(fileInclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', gulp.series('file-include'));
