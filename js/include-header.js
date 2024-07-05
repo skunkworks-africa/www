ac7379cd26f1d59683139b55fd4635cf1e4b9d15
Raydo Matthee <raydo@skunkworks.africa>
2024-07-05 06:54:06 +0200

    Update include-header.js
    Signed-off-by: Raydo Matthee <raydo@skunkworks.africa>

--- js/include-header.js
+++ js/include-header.js
@@ -1,10 +1,18 @@
 function includeHeader() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error fetching header:', error));
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-container').innerHTML = data;
+
+      // Add burger menu functionality
+      const burgerMenu = document.querySelector('.burger-menu');
+      const header = document.querySelector('.header');
+
+      burgerMenu.addEventListener('click', function () {
+        header.classList.toggle('open');
+      });
+    })
+    .catch(error => console.error('Error fetching header:', error));
 }
 
 document.addEventListener('DOMContentLoaded', includeHeader);
